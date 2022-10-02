56549a61dddac8f85ec86857bcff25dba1371bc1
jazman43 <supoww@hotmail.co.uk>
2022-10-02 18:06:59 +1300

    rename some veraibles task 1

--- CS102_A_2/Develpoment_Assessment_2_task_1/Develpoment_Assessment_2/Task_1_roots.cpp
+++ CS102_A_2/Develpoment_Assessment_2_task_1/Develpoment_Assessment_2/Task_1_roots.cpp
@@ -12,35 +12,35 @@ using namespace std;
 
 int main() {
 
-	float a, b, c, x1, x2, discriminant, realPart, imaginaryPart;
+	float numberA, numberB, numberC, x1, x2, discriminant, realPart, imaginaryPart;
 
-	cout << "Enter a : ";
-	cin >> a;
-	cout << "Enter b : ";
-	cin >> b;
-	cout << "Enter c : ";
-	cin >> c;
+	cout << "Enter number a : ";
+	cin >> numberA;
+	cout << "Enter number b : ";
+	cin >> numberB;
+	cout << "Enter number c : ";
+	cin >> numberC;
 
 
-	discriminant = b * b - 4 * a * c;
+	discriminant = numberB * numberB - 4 * numberA * numberC;
 
 	if (discriminant > 0)
 	{
-		x1 = (-b + sqrt(discriminant)) / (2 * a);
-		x2 = (-b - sqrt(discriminant)) / (2 * a);
+		x1 = (-numberB + sqrt(discriminant)) / (2 * numberA);
+		x2 = (-numberB - sqrt(discriminant)) / (2 * numberA);
 		cout << "roots are real and unequal\n";
 		cout << "x1 = " << x1 << ";  x2 = " << x2 << endl;
 	}
 	else if (discriminant == 0)
 	{
 		cout << "roots are real and equal\n";
-		x1 = -b / (2 * a);
+		x1 = -numberB / (2 * numberA);
 		cout << "x1 = " << x1 << "; x2 = " << x1 <<endl;
 	}
 	else
 	{
-		realPart = -b / (2 * a);
-		imaginaryPart = sqrt(-discriminant) / (2 * a);
+		realPart = -numberB / (2 * numberA);
+		imaginaryPart = sqrt(-discriminant) / (2 * numberA);
 		cout << "roots are imanginary\n";
 		cout << "x1 = " << realPart << "+" << imaginaryPart << "i\n";
 		cout << "x2 = " << realPart << "-" << imaginaryPart << "i\n";
