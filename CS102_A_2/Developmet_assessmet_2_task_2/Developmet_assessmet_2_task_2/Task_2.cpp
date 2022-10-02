4bd22782e40879062b4a967580ad0bb44f70f1c5
jazman43 <supoww@hotmail.co.uk>
2022-10-02 18:57:47 +1300

    adding comment to task 2

--- CS102_A_2/Developmet_assessmet_2_task_2/Developmet_assessmet_2_task_2/Task_2.cpp
+++ CS102_A_2/Developmet_assessmet_2_task_2/Developmet_assessmet_2_task_2/Task_2.cpp
@@ -5,8 +5,6 @@ task 2
 jared evans
 */
 
-
-
 #include <iostream>
 
 
@@ -19,9 +17,10 @@ void FindPrimeNumbers(int, int);
 
 int main() {
 
+	//creating vereaibles
 	int startingNum, endingNum, tempNum;
 	
-	
+	//promting user and storing vereaibles
 	cout << "enter a starting number: ";
 	cin >> startingNum;
 	cout << "enter a ending number: ";
@@ -29,7 +28,7 @@ int main() {
 
 	cout << "\n";
 
-
+	//makeing sure that starting number is always smaller
 	if (startingNum >= endingNum)
 	{
 		tempNum = startingNum;
@@ -40,24 +39,21 @@ int main() {
 
 	}
 
-
+	//providing visual info to user
 	cout << "numbers divisible by 7 from " << startingNum << " to " << endingNum << endl;
 	cout << "*************************************\n";
+	//a function that divides the numbers by 7 and displays them
+	NumbersDivisibleBy7(startingNum, endingNum);	
 
-	NumbersDivisibleBy7(startingNum, endingNum);
-	
-
+	// displays more visual info to user
 	cout << "\n";
 	cout << "\n";
 	cout << "prime Numbers\n";
 	cout << "*************\n";
 
 	cout << "Prime List between (" << startingNum << " and " << endingNum << ") :";
-
-	
-	FindPrimeNumbers(startingNum,endingNum);
-
-	
+	//a function that finds prime Numbers and displays them and total amount of prime numbers
+	FindPrimeNumbers(startingNum,endingNum);	
 
 
 	system("pause>0");
@@ -66,10 +62,10 @@ int main() {
 
 void NumbersDivisibleBy7(int startingNum, int endingNum) 
 {
-	
+	//Cycle through all numbers in between the users starting number and ending number
 	for (int number = startingNum; number <= endingNum; number++)
 	{
-
+		//then checks if it can be divided by 7 and displays that number
 		if (number % 7 == 0)
 		{
 			cout << number << ", ";
@@ -82,22 +78,23 @@ void NumbersDivisibleBy7(int startingNum, int endingNum)
 void FindPrimeNumbers(int startingNum,int endingNum) {
 
 	int primeNumberCount{};
-
+	//Cycle through all numbers in between the users starting number and ending number
 	for (int i = startingNum; i <= endingNum; i++)
 	{
 
 		bool prime = true;
-
+		//starting at 2 we cycle through the numbers a second time
 		for (int count = 2; count < i; count++)
 		{
-
+			/*Check if they are a prime if not a bool vereaible is 
+			set to false then we break out and try agin */
 			if (i % count == 0)
 			{
 				prime = false;
 				break;
 			}
 		}
-
+		//if true then we print out the prime number and add them togeter
 		if (prime)
 		{
 			cout << i << ", ";
@@ -105,7 +102,8 @@ void FindPrimeNumbers(int startingNum,int endingNum) {
 		}
 
 	}
-
-	cout << "\n" << "total number of prime numbers form (" << startingNum << " and " << endingNum << ")";
+	
+	cout << "\n" << "total number of prime numbers form (" << startingNum << " and " << endingNum << "): ";
+	//and we print the total amount of prime numbers
 	cout << primeNumberCount << endl;
 }
